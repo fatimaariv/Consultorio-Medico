70aedee8afcae9aabbf40ccd4364165db58d7ecc
fatimaariv <fatimaaa.delrivera@gmail.com>
2026-02-19 17:21:09 -0600

    conexion a tu corazon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Importas tus pantallas
 import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+// Creas el stack de navegación
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {/* Pantalla de Login: Ocultamos el encabezado para que se vea más limpio */}
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        
+        {/* Pantalla Inicial: Aquí es donde irá el usuario tras loguearse */}
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Inicio' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
